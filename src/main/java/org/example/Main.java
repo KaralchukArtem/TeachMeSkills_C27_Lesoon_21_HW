bb5f33d22b02aa03597fefba819d0fd288d06780
Karalchuk Tema <artuom441@gmail.com>
2024-04-06 22:35:26 +0300

    feat: init maven-1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Person person = new Person();
+        person.setCar("blue");
+        System.out.println(person.getCar().getColor());
     }
 }
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,9 +1,31 @@
 package org.example;
 
-import lombok.Data;
-
-@Data
 public class Person {
     private String name;
     private int age;
+    private Car car;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(String color) {
+        this.car = new Car(color);
+    }
 }
